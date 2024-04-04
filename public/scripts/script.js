387913ed3ead29bf86f2021bc186380ae8e96642
sladeberryman <slade12berryman@gmail.com>
2024-04-04 16:07:37 -0500

    closes #19

---
+++ public/scripts/script.js
@@ -0,0 +1,25 @@
+// for adding ingredients and steps in the create recipe form
+document.addEventListener("DOMContentLoaded", function() {
+    const addIngredientBtn = document.getElementById("addIngredientBtn");
+    const addStepBtn = document.getElementById("addStepBtn");
+    const ingredientsContainer = document.getElementById("ingredientsContainer");
+    const stepsContainer = document.getElementById("stepsContainer");
+  
+    addIngredientBtn.addEventListener("click", function() {
+      const ingredientInput = document.createElement("input");
+      ingredientInput.type = "text";
+      ingredientInput.className = "form-control mb-2";
+      ingredientInput.name = "ingredients[]";
+      ingredientInput.required = false;
+      ingredientsContainer.appendChild(ingredientInput);
+    });
+  
+    addStepBtn.addEventListener("click", function() {
+      const stepTextArea = document.createElement("textarea");
+      stepTextArea.className = "form-control form-control-sm mb-2";
+      stepTextArea.name = "steps[]";
+      stepTextArea.rows = 3;
+      stepTextArea.required = false;
+      stepsContainer.appendChild(stepTextArea);
+    });
+  });
